364448054987bfaf92e326f4bfcea2da22067674
ashokk <ashokk@nextgen.com>
2022-11-27 14:53:57 +0530

    added regex basics

---
+++ Patterns.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegexPattern
+{
+    /*
+    ^ -             is starting pointing of regex pattern.
+    $ -             ending point of regex pattern.
+    [0-9a-zA-Z] brackets can be used to mention permissible.
+    \+
+    \s                 to have a whitespace
+    |                   or pattern
+    []{3}              permissible charactors with 3 count
+      []{3,}          permissible charactors atleast 3 times
+    []{3,10}        min 3 chars and max 10
+    *	*?	            Used to match the preceding character zero or more times.
++	+?	            Used to match the preceding character one or more times.
+?	??	                Used to match the preceding character zero or one time.
+{n}	{n}?	        Used to match the preceding character exactly n times.
+{n, }	{n, }?	    Used to match the preceding character at least n times.
+{n, m}	{n, m}?	Used to match the preceding character from n to m times.
+     */
+    public class Patterns
+    {
+        public static string PinCodePattern = "^[0-9]{6}$";
+        /*
+         //   Format                Pattern
+        // xxxxxxxxxx           ^[0 - 9]{ 10}$
+        // xxx - xxxx - xxxx   ^[0 - 9]{ 3} -[0 - 9]{ 4}-[0 - 9]{ 4}$
+         */
+        public static string PhoneNumberPattern = @"(^[0-9]{10}$)|(^[0-9]{3}-[0-9]{4}-[0-9]{4}$)";
+
+        // ashok.andani007@gmail.com
+        public static string EmailPattern = @"[a-z]{3,7}.?[a-z0-9]{3,8}@[a-z]{3,8}.(com|org)$";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// for Pincode
+            //Console.WriteLine("Enter PinCode:");
+            //string pincode=Console.ReadLine();
+            //Regex Pincoderegex = new Regex(Patterns.PinCodePattern);
+            //var Pincodeflag= Pincoderegex.IsMatch(pincode);
+            //Console.WriteLine(Pincodeflag);
+
+            //// for phone number
+            //Console.WriteLine("Enter Phone Number:");
+            //string PhoneNumber = Console.ReadLine();
+            //Regex phoneRegex = new Regex(Patterns.PhoneNumberPattern);
+            //var phoneFlag = phoneRegex.IsMatch(PhoneNumber);
+            //Console.WriteLine(phoneFlag);
+
+            // for gmail
+            Console.WriteLine("Enter mail id:");
+            string MailId = Console.ReadLine();
+            Regex mailRegex = new Regex(Patterns.EmailPattern);
+            var mailFlag = mailRegex.IsMatch(MailId);
+            Console.WriteLine(mailFlag);
+        }
+    }
+}
